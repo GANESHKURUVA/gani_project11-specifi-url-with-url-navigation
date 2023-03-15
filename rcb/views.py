28f6b5ab4607a1ca2447c30eeb472fbf1868fd56
GANESHKURUVA <kuruvaganesh07@gmail.com>
2023-03-15 23:00:41 +0530

    first commit

---
+++ rcb/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def virat(request):
+    return render(request,'virat.html')
+
+
+def duplessis(request):
+    return render(request,'duplessis.html')
